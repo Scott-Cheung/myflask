5b3eb58231397cdb35af1ccd20ea7f91bf99ff91
Troy Chueng <804786173@qq.com>
2017-08-02 17:04:08 +0800

    day01

---
+++ myflask.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask,url_for,render_template
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',title=u'主页')
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title=u'关于')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
